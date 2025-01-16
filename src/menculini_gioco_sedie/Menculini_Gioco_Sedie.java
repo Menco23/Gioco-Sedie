ec9d031cca46e76704f073fa0d70950d0da6de18
Menco23 <menculinitommaso1@gmail.com>
2025-01-16 11:42:32 +0100

    implementazione della classe scrittore

--- src/menculini_gioco_sedie/Menculini_Gioco_Sedie.java
+++ src/menculini_gioco_sedie/Menculini_Gioco_Sedie.java
@@ -1,25 +1,36 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package menculini_gioco_sedie;
 
+import java.util.logging.Logger;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  *
- * @author MC
+ * @author Menculini Tommaso
  */
-public class Menculini_Gioco_Sedie {
+public class Menculini_Gioco_Sedie { 
     private final static int NUMSEDIE = 15;
     private static Logger logger = Logger.getLogger("GiocoSedie.TestGiocoSedie");
+    String nomeFile;
     
     /**
      * @param args the command line arguments
+     * 
      */
     public static void main(String[] args) {
+        
+        Scrittore scrittore1 = new Scrittore("Risultato.txt");
+       
+    
+        
+        
         Posto sedie[] = new Posto[NUMSEDIE];
 
 	for (int k = 0; k < sedie.length; k++)
@@ -36,8 +47,9 @@ public class Menculini_Gioco_Sedie {
                 //System.out.println("Sto facendo partire il thread n." + array[i].getId());
                 logger.info("Sto facendo partire il thread id: " + array[i].getId()+" name: "+array[i].getName()+"\n");
                 array[i].start();
+          
                 }
+          System.out.println("Il Thread sta scrivendo il File.");
 	}
-    }
- 
-
+      }
+        
---
+++ src/menculini_gioco_sedie/Scrittore.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package menculini_gioco_sedie;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Menculini Tommaso
+ */
+
+public class Scrittore {
+
+    String nomeFile;
+  
+    
+    public Scrittore(String nomeFile){
+        this.nomeFile = nomeFile;
+    }
+    
+    /**
+     * Scrive un file di testo usando la classe BufferedWriter
+     * @param Id
+     * @param posto
+     */
+    public void scrivi(int Id, int posto){
+        BufferedWriter br=null;
+        
+        try {
+            //1) apro il file
+            br = new BufferedWriter(
+                    new FileWriter(nomeFile));
+            //2) scrivo nel buffer
+            br.write("IL thread "+ Id + "occupa il posto "+ posto );
+            br.write("\n\r");
+            //3) svuoto il buffer e salvo nel file i dati
+            br.flush();         
+        } catch (IOException ex) {
+            Logger.getLogger(Scrittore.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally{
+            if (br!=null)
+                try {
+                    //4)chiudo lo stream in uscita
+                    br.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Scrittore.class.getName()).log(Level.SEVERE, null, ex);
+            }
+                
+        }
+    }
+}
